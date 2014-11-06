495030592f7d8e22c86e5ce4740f7a83440cb085
JP Simard <jp@jpsim.com>
2014-11-06 15:33:49 -0800

    Initial Commit

---
+++ PeerKit/PeerKit.h
@@ -0,0 +1,19 @@
+//
+//  PeerKit.h
+//  PeerKit
+//
+//  Created by JP Simard on 11/6/14.
+//  Copyright (c) 2014 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PeerKit.
+FOUNDATION_EXPORT double PeerKitVersionNumber;
+
+//! Project version string for PeerKit.
+FOUNDATION_EXPORT const unsigned char PeerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeerKit/PublicHeader.h>
+
+
